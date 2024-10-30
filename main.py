2becd04622c3d65378d5541b3c79a3e30b846903
worka <study.aniket.hingane@gmail.com>
2024-10-30 05:57:42 -0400

    lib deps setup

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import pm4py
+
+df = pd.read_csv('data/running_example.csv', sep=';')
+print(df)
+
+
+log = pm4py.format_dataframe(df, case_id='case_id',activity_key='activity',
+                             timestamp_key='timestamp')
+
+print(log)
+
+print(pm4py.get_start_activities(log))
+print(pm4py.get_end_activities(log))
+
+
+# final process map
+log.to_csv('output/running_example_exported.csv')
